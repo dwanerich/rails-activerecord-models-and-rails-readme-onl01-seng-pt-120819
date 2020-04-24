310672b9d8dc5259d051a5dff69d082a2c3b98f0
dwane richards <dwanerich@gmail.com>
2020-04-24 00:57:08 +0000

    Automatically backed up by Learn

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Post do
   it 'can be created' do
     post = Post.create!(title: "My title", description: "The post description")
-    except(post).to be_valid
+    expect(post).to be_valid
   end
   
 end
